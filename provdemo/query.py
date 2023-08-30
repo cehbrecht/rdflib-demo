293a285667572b2ac396a1f6b188437381be0127
Pingu Carsti <ehbrecht@dkrz.de>
2023-08-30 17:19:00 +0200

    refactor query

--- provdemo/query.py
+++ provdemo/query.py
@@ -2,14 +2,16 @@ from provdemo.db import GraphDB
 
 
 def query_all():
-    graph_db = GraphDB()
-   
     query_str = """
         SELECT ?subject ?predicate ?object
         WHERE {
         ?subject ?predicate ?object
         }
     """
+    _query(query_str)
+
+def _query(query_str):
+    graph_db = GraphDB()
     results = graph_db.query(query_str)
 
     # Print the query results
