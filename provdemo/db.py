e4a68a79a3cc6cf0d2d47707a25acbf0b73b4f9a
Pingu Carsti <ehbrecht@dkrz.de>
2023-08-30 16:08:55 +0200

    renamed db2 to db

--- provdemo/db.py
+++ provdemo/db.py
@@ -1,76 +1,41 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, Integer, String, Text
-from rdflib import Graph
-from rdflib.plugins.sparql import prepareQuery
-
+from rdflib import Graph, URIRef, Literal
+from rdflib.namespace import RDF
 
 # Provide the path to the SQLite database in the local folder
-database_path = "provenance_database.sqlite"
-
-Base = declarative_base()
-
-class RDFGraph(Base):
-    __tablename__ = 'rdf_graph'
-    
-    id = Column(Integer, primary_key=True)
-    graph_data = Column(Text)
+db_url = "sqlite:///my_rdf_store.db"
 
 class GraphDB(object):
     def __init__(self):
-        # Create a SQLAlchemy engine
-        self.engine = create_engine(f'sqlite:///{database_path}', echo=True)
+        # Create a graph with a specific backend store
+        self.graph = Graph(
+            store="SQLAlchemy", 
+            identifier=URIRef("http://example.org/graph"))
+        self.graph.open(db_url, create=True)
 
-        # Create the table(s) in the database
-        Base.metadata.create_all(self.engine)
+    def add(self, data):
+        new_graph = Graph()
+        new_graph.parse(data=data, format="turtle")
 
-        # Create a session factory
-        Session = sessionmaker(bind=self.engine)
-        self.session = Session()
-
-    def add(self, rdf_graph):
-        # Create an instance of your data model
-        rdf_graph_instance = RDFGraph(graph_data=rdf_graph)
-
-        # Add the instance to the session
-        self.session.add(rdf_graph_instance)
-
-        # Commit the changes to the database
-        self.session.commit()
-
-        # Close the session when done
-        self.session.close()
+        # add rdf to existing graph
+        print("\n\nadd new graph")
+        for triple in new_graph:
+            print(triple)
+            self.graph.add(triple)
+        # Commit changes to the store
+        self.graph.store.commit()
 
     def query(self):
-        # Retrieve the RDF graph data from the database
-        rdf_graph_instance = self.session.query(RDFGraph).first()
+        # Define and execute a SPARQL query
+        query = """
+            SELECT ?s ?p ?o
+            WHERE {
+                ?s ?p ?o .
+            }
+        """
 
-        if rdf_graph_instance:
-            rdf_graph_data = rdf_graph_instance.graph_data
-    
-            # Create an RDF graph and parse the data
-            rdf_graph = Graph()
-            rdf_graph.parse(data=rdf_graph_data, format="turtle")
-    
-            # Define a SPARQL query
-            query = prepareQuery(
-                """
-                SELECT ?s ?p ?o WHERE {
-                    ?s ?p ?o .
-                }
-                """,
-                initNs={"ex": "http://example.org/"}
-            )
-    
-            # Execute the SPARQL query
-            results = rdf_graph.query(query)
-    
-            # Print the query results
-            for row in results:
-                print(row)
-        else:
-            print("No RDF graph data found in the database.")
+        results = self.graph.query(query)
 
-        # Close the session when done
-        self.session.close()
+        # Print the query results
+        print(f"\n\nquery: results={len(results)}")
+        for row in results:
+            print(row)
