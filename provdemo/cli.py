89bd9f17366550a494e66e773a83269a4ce32561
Pingu Carsti <ehbrecht@dkrz.de>
2023-08-30 15:48:53 +0200

    update rdf example

--- provdemo/cli.py
+++ provdemo/cli.py
@@ -4,11 +4,11 @@ from provdemo.provenance import Provenance
 def cli():
     prov = Provenance()
     prov.build()
-    rdf_graph = prov.get_rdf_graph()
+    rdf_data = prov.get_rdf_graph()
 
     # write to store
     graph_db = GraphDB()
-    graph_db.add(rdf_graph)
+    graph_db.add(rdf_data)
 
     # query store
     graph_db.query()
--- provdemo/db2.py
+++ provdemo/db2.py
@@ -1,6 +1,5 @@
-from rdflib import Graph, URIRef
-from rdflib_sqlalchemy import registerplugins
-from sqlalchemy import create_engine
+from rdflib import Graph, URIRef, Literal
+from rdflib.namespace import RDF
 
 # Provide the path to the SQLite database in the local folder
 db_url = "sqlite:///my_rdf_store.db"
@@ -10,12 +9,28 @@ class GraphDB(object):
         # Create a graph with a specific backend store
         self.graph = Graph(
             store="SQLAlchemy", 
-            identifier=URIRef("http://example.org/graph1"))
+            identifier=URIRef("http://example.org/graph"))
         self.graph.open(db_url, create=True)
 
-    def add(self, rdf_graph):
+        # Add some data to the graph
+        self.graph.add((URIRef("http://example.org/subject1"), RDF.type, URIRef("http://example.org/Person")))
+        self.graph.add((URIRef("http://example.org/subject1"), URIRef("http://example.org/name"), Literal("Alice")))
+
+        # Commit the changes to the database
+        self.graph.commit()
+
+    def add(self, data):
+        return
         new_graph = Graph()
-        new_graph.parse(data=rdf_graph, format="turtle")
+        new_graph.parse(data=data, format="turtle")
+        # dummy
+        new_graph = Graph()
+
+        # Add some data to the graph
+        new_graph.add((URIRef("http://example.org/subject1"), RDF.type, URIRef("http://example.org/Person")))
+        new_graph.add((URIRef("http://example.org/subject1"), URIRef("http://example.org/name"), Literal("Alice")))
+
+
         # add rdf to existing graph
         for triple in new_graph:
             print(triple)
@@ -24,12 +39,18 @@ class GraphDB(object):
         self.graph.store.commit()
 
     def query(self):
-        # You can now run SPARQL queries on the stored data
+        # Define and execute a SPARQL query
         query = """
-            SELECT ?s ?p ?o
+            SELECT ?subject ?name
             WHERE {
-                ?s ?p ?o .
+                ?subject a <http://example.org/Person> .
+                ?subject <http://example.org/name> ?name .
             }
         """
-        # for row in self.graph.query(query):
-        #    print(row)
+
+        results = self.graph.query(query)
+
+        # Print the query results
+        for row in results:
+            subject, name = row
+            print(f"Subject: {subject}, Name: {name}")
