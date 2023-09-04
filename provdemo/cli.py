ec56f9096e3492c9894195fcd056d70b1fec0b48
Carsten Ehbrecht <ehbrecht@dkrz.de>
2023-09-04 13:21:20 +0200

    storing stats

--- provdemo/cli.py
+++ provdemo/cli.py
@@ -4,14 +4,21 @@ from provdemo import report
 from datetime import datetime
 
 def cli():
+    print("build prov")
     prov = build()
 
     # store in db and file
+    print("store prov")
     prov.store_rdf()
     prov.write_rdf()
 
     # query
+    print("query prov")
     df = query.query()
+    print(df)
+
+    # report
+    print("report prov")
     report.write_html(df)
 
 def build():
@@ -24,17 +31,14 @@ def build():
         {
             "dataset_name": "HadCRUT5",
             "variable_name": "tas_mean",
+            "min": 10,
+            "max": 20,
+            "stddev": 2,
         }, 
         ["http://example.org/input/HadCRUT.5.0.1.0.anomalies.ensemble_mean.nc"], 
         ["http://example.org/output/HadCRUT.5.0.1.0.anomalies.ensemble_mean_infilled.nc"],
         start_time,
-        end_time
-    )
-    prov.add_statistics(
-        ["http://example.org/output/HadCRUT.5.0.1.0.anomalies.ensemble_mean_infilled.nc"], 
-        10, 
-        20, 
-        2
+        end_time,
     )
     return prov
 
--- provdemo/query.py
+++ provdemo/query.py
@@ -4,26 +4,30 @@ import pandas as pd
 
 def query():
     query_str = """
-        SELECT ?process ?dataset ?variable ?startTime ?endTime ?input ?output
+        SELECT ?process ?dataset ?variable ?startTime ?endTime ?input ?output ?min ?max ?stddev
         WHERE {
             ?exec rdf:type provone:Execution ;
                 rdfs:label ?process ;
                 clint:dataset_name ?dataset ;
                 clint:variable_name ?variable ;
                 prov:startedAtTime ?startTime ;
-                prov:endedAtTime ?endTime .
-            
+                prov:endedAtTime ?endTime ;
+                clint:min ?min ;
+                clint:max ?max ;
+                clint:stddev ?stddev .
+
             ?input rdf:type prov:Entity .
         
             ?output rdf:type prov:Entity ;
                 prov:qualifiedDerivation [ prov:entity ?input; prov:hadActivity ?exec ] .
-        }
+    }
     """
     graph_db = GraphDB()
     results = graph_db.query(query_str)
 
     data = []
     for row in results:
+        # print(row)
         process = row.process.split("/")[-1]
         dataset = row.dataset.value
         variable = row.variable.value
@@ -33,6 +37,9 @@ def query():
         input = input.split("urn:clint:")[-1]
         output = row.output.split("/")[-1]
         output = output.split("urn:clint:")[-1]
+        min = row.min.value
+        max = row.max.value
+        stddev = row.stddev.value
         data.append({
             "Process": process, 
             "Dataset": dataset,
@@ -40,7 +47,10 @@ def query():
             "Start Time": start_time, 
             "End Time": end_time,
             "Input": input,
-            "Output": output
+            "Output": output,
+            "Min": min,
+            "Max": max,
+            "StdDev": stddev,
         })
     df = pd.DataFrame(data)
     return df
